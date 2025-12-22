03bf513e5aa34ff26d8546244af63a26813eace7
Vadim <vadimchubok@gmail.com>
2025-12-22 16:37:40 +0200

    Added fix to settings

--- movie_marathon/settings.py
+++ movie_marathon/settings.py
@@ -1,6 +1,8 @@
 import os
+import cloudinary
 from pathlib import Path
 from dotenv import load_dotenv
+
 
 
 load_dotenv()
@@ -90,7 +92,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+cloudinary.config(
+    cloud_name="dkfdxjbww"
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
